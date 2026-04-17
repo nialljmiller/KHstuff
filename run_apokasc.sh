e90f65c7457be0b1c64aa1bc6b015197e4f8bdb3
Niall Miller <niall.j.miller@gmail.com>
2026-04-17 14:50:50 -0600

    Update run_apokasc.sh

--- run_apokasc.sh
+++ run_apokasc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=apokasc_bananas
-#SBATCH --array=0-2999
+#SBATCH --array=0-2056%20
 #SBATCH --time=00:10:00
 #SBATCH --mem=4G
 #SBATCH --output=logs/apokasc_%a.out
@@ -15,4 +15,4 @@
 
 source ~/python_projects/venv/bin/activate
 cd /project/galacticbulge/kiauhoku_NJM/KHstuff
-python make_apokasc_bananas.py --star_index $SLURM_ARRAY_TASK_ID
+python make_apokasc_bananas.py --star_index $SLURM_ARRAY_TASK_ID --n_walkers 24 --n_burnin 200 --n_iter 600
